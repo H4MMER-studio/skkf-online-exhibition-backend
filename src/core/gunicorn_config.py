d31971b208fe956caf41a9dc4ed3eeb48d359001
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-06-10 23:38:30 +0900

    :wrench: Add gunicorn configuration file

--- src/core/gunicorn_config.py
+++ src/core/gunicorn_config.py
@@ -1,8 +1,11 @@
 import multiprocessing
 
-workers: int = multiprocessing.cpu_count() * 2 + 1
-worker_class: str = "uvicorn.workers.UvicornWorker"
+bind = ["0.0.0.0:80"]
+daemon = True
 
-loglevel: str = "debug"
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "uvicorn.workers.UvicornWorker"
+
+loglevel = "debug"
 accesslog = "/home/ubuntu/build/logs/access.log"
 errorlog = "/home/ubuntu/build/logs/error.log"
